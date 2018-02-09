3553fddbcb582cc311fb6fb62442675f9cfb8476
Steven Ciasullo <sjciasullo@gmail.com>
2018-02-09 14:18:18 -0700

    protect for bugs in changing zip code

--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
 function validateZip(zipcode) {
+  console.log(zipcode);
   const api = 'AIzaSyAX6KJo_fn4rvGYMbZ_ZBpm9vDiDIVZTHk';
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&${api}`;
-  $.get(url).done(function(response) {
+  $.get(url).success(function(response) {
     // display the address specified by zipcode
-    const address = response.results[0].formatted_address;
-    $zipAddress = $(`<p id='address-check'>${address}</p>`)
-    $("#zip").after($zipAddress);
-
-    // if the address is in USA, select the state by the result and show submit button
-    if(address.includes("USA")){
-      response.results[0].address_components.forEach( function(comp) {
-        if(comp.types[0] == "administrative_area_level_1") {
-          let state = comp.short_name;
-          $("#state").val(state)
+    if(response.status === 'OK') {
+      const address = response.results[0].formatted_address;
+      let $address = $("#address-check");
+      if($address.length !== 0){
+        $address.text(address);
+      } else {
+        const $zipAddress = $(`<p id='address-check'>${address}</p>`)
+        $("#zip").after($zipAddress);
+      }
+      
+  
+      // if the address is in USA, select the state by the result and show submit button
+      if(address.includes("USA")){
+        response.results[0].address_components.forEach( function(comp) {
+          if(comp.types[0] == "administrative_area_level_1") {
+            let state = comp.short_name;
+            $("#state").val(state)
+          }
+        })
+  
+        //and reveal submit button
+        $submit = $("#submit");
+        if ($submit.hasClass("hidden")){
+          $submit.toggleClass("hidden");
         }
-      })
-
-      //and reveal submit button
-      $("#submit").toggleClass("hidden");
+      }
     }
     
   })
