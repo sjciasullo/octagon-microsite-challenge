01133cb1cec88864dbb19082cf2dcd69572ad4be
Steven Ciasullo <sjciasullo@gmail.com>
2018-02-09 13:41:35 -0700

    add functionality to validate zip code, display address and select state

--- src/main.js
+++ src/main.js
@@ -2,19 +2,30 @@ function validateZip(zipcode) {
   const api = 'AIzaSyAX6KJo_fn4rvGYMbZ_ZBpm9vDiDIVZTHk';
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&${api}`;
   $.get(url).done(function(response) {
-    //prints the state
-    const state = response.results[0].address_components[3].short_name
-    console.log(state);
-    $("#state").val(event.target.value)
+    // display the address specified by zipcode
+    const address = response.results[0].formatted_address;
+    $zipAddress = $(`<p id='address-check'>${address}</p>`)
+    $("#zip").after($zipAddress);
+
+    // if the address is in USA, select the state by the result
+    if(address.includes("USA")){
+      response.results[0].address_components.forEach( function(comp) {
+        if(comp.types[0] == "administrative_area_level_1") {
+          let state = comp.short_name;
+          $("#state").val(state)
+        }
+      })
+    }
+    
   })
 }
 
 $(document).on('ready', function(){
-  console.log('main.js connected');
+  //add event litener to zip input
   $("#zip").bind('input', function(event) {
-    console.log(event.target.value);
-    if(event.target.value == '123') {
-      $("#state").val('CT');
+    //only check 5 digit zip codes
+    if(event.target.value.length == 5) {
+      validateZip(event.target.value);
     }
   })
 })
