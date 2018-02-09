62e1709ba8608f743a2eccc80b970c81cdea898d
Steven Ciasullo <sjciasullo@gmail.com>
2018-02-09 12:37:34 -0700

    simple setting zipcode autoselects state

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ function validateZip(zipcode) {
   const api = 'AIzaSyAX6KJo_fn4rvGYMbZ_ZBpm9vDiDIVZTHk';
   const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipcode}&${api}`;
   $.get(url).done(function(response) {
-    console.log(response);
+    //prints the state
+    const state = response.results[0].address_components[3].short_name
+    console.log(state);
+    $("#state").val(event.target.value)
   })
 }
 
 $(document).on('ready', function(){
-  console.log("main.js connected");
-  validateZip("48104");
+  console.log('main.js connected');
+  $("#zip").bind('input', function(event) {
+    console.log(event.target.value);
+    if(event.target.value == '123') {
+      $("#state").val('CT');
+    }
+  })
 })
